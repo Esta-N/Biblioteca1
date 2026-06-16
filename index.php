92ec7053b39f508d45d582185080ff8c7b239a20
Esta-N <tsltn14@gmail.com>
2026-06-16 18:16:25 -0300

    index?

--- index.php
+++ index.php
@@ -10,17 +10,6 @@
         $Prestamo1 = new Prestamo($Socio1, $Libro1, "1", "2");
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Index</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
     <?php include('header.php'); ?>
     <h1 class="h1">Hola Causa</h1>
     <div class="container">
@@ -60,5 +49,3 @@
         </div>
     </div>
     <?php include('footer.php'); ?>
-</body>
-</html>
