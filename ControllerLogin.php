0f672472a690f200553e2344a613f8eedeb624d0
Esta-N <tsltn14@gmail.com>
2026-06-23 18:36:02 -0300

    controllerLogin fix

--- ControllerLogin.php
+++ ControllerLogin.php
@@ -2,10 +2,12 @@
 $usuario1 = "vegeta";
 $password1 = 777;
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (!empty($_POST['usuario']) && !empty($_POST['password']) && 'usuario' == $usuario1 && 'password' == $password1) {
-            $usuario = htmlspecialchars($_POST['usuario']);
-            echo "Bienvenido, " . $usuario;
-            header("Location: index.php");
+        if (!empty($_POST['usuario']) && !empty($_POST['password'])) {
+            if (($_POST['usuario'] == $usuario1) && ($_POST['password'] == $password1)) {
+                $usuario = htmlspecialchars($_POST['usuario']);
+                echo "Bienvenido, " . $usuario;
+                header("Location: index.php");
+            }
         } else {
             echo "Por favor, completa todos los campos.";
         }
